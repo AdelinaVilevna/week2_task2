7d57ed693336e239b6da772b314b35b5318e43d9
AdelinaVilevna <adelina.abdykanyeva@gmail.com>
2020-11-02 23:19:33 +0600

    week2_task2

---
+++ Abdykanyeva_task3.py
@@ -0,0 +1,46 @@
+# Question 1
+
+#  1- way
+# string_name = input("Please enter your input: ")
+# print(len(string_name))
+
+# 2-way
+# a = "Hello world!"
+# print(len(a))
+
+# Question 2
+# string_name = ("hello world!") .upper()
+# print(string_name)
+
+# string_name = ("HELLO WORLD!") .lower()
+# print(string_name)
+
+#Question 3
+# string_name = ("hello world!") .replace("h","$"). replace("!", "$")
+# print(string_name)
+
+# Question 4
+# string_name = input("Please enter your greetings: ")
+# print(string_name.split(" "))
+
+
+# Question 5
+
+# string_name = input("Enter your name: ")
+# print("Hello," + string_name,"!")
+
+#Question 6
+# string_name = "Hello world I'm backend developer!"
+# print (len(string_name.split()))
+
+#Question 7
+# string_name = "hello world!"
+# print (string_name[3])
+# print (string_name[-2])
+# print (string_name[0:6])
+# print (string_name[0:10])
+# print (string_name[::2])
+# print (string_name[1::2])
+# print (string_name[::-1])
+# print (string_name[::-2])
+# print (len(string_name))
